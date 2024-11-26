aaacba9359466ebe034962fd5c32fa8302dc0c9c
Billy207 <gelso.junior@simrede.com.br>
2024-11-26 15:00:30 -0300

    Trabalhos do Billy
    Trabalhos do Billy

---
+++ principal.js
@@ -0,0 +1,15 @@
+function fazGet(url) {
+let request = XMLHttpRequest()
+request.open("GET", url, false)
+request.send()
+return request.responseText 
+
+
+}
+function criaLinha(usuario) {
+
+
+}
+ function main() {
+    usuarios = fazGet(https://dog.ceo/api/breeds/image/random)
+ }
